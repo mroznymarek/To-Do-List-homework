e3b1861e93e59e094c67a07ccf535085c6a144af
Marek Mróz <marekmroz@wp.pl>
2019-03-31 21:23:39 +0200

    input, button added

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-class toDoList {
-    constructor(selector){
-        this.container = document.querySelector(selector) || document.body
-        this.tasks = []
-        this.render()
+class ToDoList {
+    constructor(selector) {
+        this.container = document.querySelector(selector) || document.body;
+        this.toDo = [];
+        this.render();
     }
+
+    render(){
+        this.renderForm();
+    }
+
+    renderForm() {
+        const div = document.createElement('div');
+        const input = document.createElement('input');
+        const button = document.createElement('button');
+
+        div.classList.add('form-container');
+        button.innerText = 'ADD';
+
+        div.appendChild(input);
+        div.appendChild(button);
+        this.container.appendChild(div);
+    }
+
+} 
